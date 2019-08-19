2d9acad92001f572f552631d81e01f7e5bdb8846
wangyh <1037790353@qq.com>
2019-08-19 10:45:05 +0800

    使用localStroage存储memo

--- src/store/modules/memo.js
+++ src/store/modules/memo.js
@@ -1,18 +1,29 @@
+function getMemoList() {
+  try {
+    if (localStorage.getItem("memoList")) {
+      return JSON.parse(localStorage.getItem("memoList"));
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 const state = {
-  memoList: [], //memo列表
+  memoList: getMemoList(), //memo列表
   thisMemo: {} //memo详细
 };
-const getters = {
-};
+const getters = {};
 const mutations = {
   /* 增 */
   addMemo(state, payload) {
     state.memoList.unshift(payload);
+    localStorage.setItem("memoList", JSON.stringify(state.memoList));
     state.thisMemo = payload;
   },
   /* 删 :1.根据id删除，2.删除选中*/
   deleteMemo(state, payload) {
-    for (let key=0; key < state.memoList.length; key++) {
+    for (let key = 0; key < state.memoList.length; key++) {
       const element = state.memoList[key];
       //如果payload是一个数组，则为删除选中的memo
       if (typeof payload == "object") {
@@ -30,24 +41,26 @@ const mutations = {
         }
       }
     }
+    localStorage.setItem("memoList", JSON.stringify(state.memoList));
     //将编辑内容置为空
     state.thisMemo = {};
   },
   /* 删除所有 */
   deleteAll(state) {
     state.memoList = [];
+    localStorage.removeItem("memoList");
   },
   /* 改 */
   updateMemo(state, payload) {
     for (const key in state.memoList) {
       if (state.memoList.hasOwnProperty(key)) {
         const element = state.memoList[key];
-        if(element.id==payload.id){
-          state.memoList[key]=payload;
+        if (element.id == payload.id) {
+          state.memoList[key] = payload;
         }
       }
     }
-    
+    localStorage.setItem("memoList", JSON.stringify(state.memoList));
   },
   /* 根据id查询memo */
   queryMemoById(state, payload) {
@@ -55,7 +68,7 @@ const mutations = {
       if (memo.id == payload) {
         state.thisMemo = memo;
         return true;
-      }else{
+      } else {
         state.thisMemo = {};
         return false;
       }

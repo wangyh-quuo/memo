466d45f5dadfddb791e3065233d68f428eb95afd
wangyh <1037790353@qq.com>
2019-08-21 12:04:02 +0800

    build

---
+++ dist/precache-manifest.62459519eb99b413d8346455c64c51c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "887881f9c582f56a41cc",
+    "url": "js/chunk-vendors.acceb144.js"
+  },
+  {
+    "revision": "929616e8e48ab43da5c4",
+    "url": "js/app.1452440d.js"
+  },
+  {
+    "revision": "7bd4581348210c6ad044",
+    "url": "js/about.a27e76ec.js"
+  },
+  {
+    "revision": "bb15cceacfde09e046eb22a343b39da4",
+    "url": "index.html"
+  },
+  {
+    "revision": "dea73a69b760f3beb0a5a5d8a6862f20",
+    "url": "img/iconfont.dea73a69.svg"
+  },
+  {
+    "revision": "929616e8e48ab43da5c4",
+    "url": "css/app.5adb2ef8.css"
+  },
+  {
+    "revision": "7bd4581348210c6ad044",
+    "url": "css/about.ab04cc45.css"
+  }
+];
